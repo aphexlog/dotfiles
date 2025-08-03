4449071a4d8a7e749d3ac1cd5f4aa27fe0cc433e
Aaron West <aphexlog@github.com>
2025-08-03 13:15:20 -0500

    updating wezterm BG image path

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -22,7 +22,7 @@ config.leader = { key = "w", mods = "CTRL", timeout_milliseconds = 1000 }
 config.background = {
 	{
 		source = {
-			File = "/Users/aphexlog/Pictures/terminal/space.jpg",
+			File = wezterm.home_dir .. "/space.jpg",
 		},
 		hsb = dimmer,
 		attachment = { Parallax = 0.1 },
