cef022eca6c22fa0045b4fc0547645ecb033ad94
Aaron West <aphexlog@github.com>
2025-08-11 18:37:01 -0500

    updating nvim so no color is overriden

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,5 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 vim.opt.shell = "/bin/zsh"
--- vim.opt.shellcmdflag = "-c"
--- vim.env.SHELL = "/bin/zsh"
+vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "NONE" })
